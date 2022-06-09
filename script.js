35922019cf94a288acc3165a5bbb96d82ad385da
hishanto <husain.imambd@gmail.com>
2022-06-09 16:45:38 +0600

    Js is added

---
+++ script.js
@@ -0,0 +1,7 @@
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById('btn');
+const btn = document.querySelector('.color');
+
+
+
+btn.addEventListener
