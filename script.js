a6d26715e5d6dddfd4067f1718116f4d2ebc39c6
hishanto <husain.imambd@gmail.com>
2022-06-10 12:29:53 +0600

    JS- random Value generator

--- script.js
+++ script.js
@@ -6,7 +6,12 @@ const color = document.querySelector('.color');
 //Add event listener to the button
 btn.addEventListener('click', function () {
     // console.log(document.body);
-    const randomNumber = 2;
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber);
     document.body.style.backgroundColor = colors[randomNumber];
     color.textContent = colors[randomNumber];
 });
+//Random Number generator
+function getRandomNumber() {
+    return Math.floor(Math.random()* colors.length);
+}
