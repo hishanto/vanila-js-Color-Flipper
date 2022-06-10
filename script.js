0ec6b5460da51839f6197d1ca081add181d2c9be
hishanto <husain.imambd@gmail.com>
2022-06-10 11:55:03 +0600

    js-  hard code color for the body in eventlistener

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 const btn = document.getElementById('btn');
-const btn = document.querySelector('.color');
+const color = document.querySelector('.color');
 
 
-
-btn.addEventListener
+//Add event listener to the button
+btn.addEventListener('click', function () {
+    // console.log(document.body);
+    const randomNumber = 2;
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+});
